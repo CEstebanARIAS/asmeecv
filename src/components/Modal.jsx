0aa83408bad754572b2ddd64502d154231c59091
Esteban Arias <carlos.caro199987@gmail.com>
2024-06-30 23:20:29 -0500

    creacion del componente modal, para mostrar mas informacion en la pagina

---
+++ src/components/Modal.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from "react-bootstrap/Modal";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export function ButtonModal({title, body, button}) {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        {button}
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>{title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{body}</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Cerrar
+          </Button>
+          {/* <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button> */}
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+
